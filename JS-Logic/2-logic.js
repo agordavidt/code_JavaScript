3c00caec050594b87011d3dabceb5da0fac9dbc7
agordavidt <98212267+agordavidt@users.noreply.github.com>
2022-12-29 19:45:11 +0100

    commit

--- JS-Logic/2-logic.js
+++ JS-Logic/2-logic.js
@@ -1,7 +1,7 @@
 let x = 5;
 
 if (x > 0) {
-    console.log('x is a positive number');
+    console.log(`${x} is a positive number`);
 } else {
-    console.log('x is a negative number');
+    console.log(`${x} is a negative number`);
 }
