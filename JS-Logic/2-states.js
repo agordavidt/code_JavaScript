afd8c87402851672874bc333e4993166e07dd8a0
agordavidt <98212267+agordavidt@users.noreply.github.com>
2022-12-29 19:32:25 +0100

    for loop

---
+++ JS-Logic/2-states.js
@@ -0,0 +1,6 @@
+let states = ['Rivers', 'Lagos', 'Anambra', 'Benue', 'Pleateau', 'Kogi', 'Oyo', 'Enugu', 
+'Delta', 'Bayelsa', 'Edo', 'Abia', 'Ebonyi', 'Imo', 'Kaduna', 'Cross river', 'Akwa Ibom', 'Nassarawa', 'Ogun', 'Gombe'];
+
+for (let i = 0; i < states.length; i++){
+    console.log("Peter Obi will win " + states[i] + " state");
+}
